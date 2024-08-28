2a2bbb40315daae7afeb45127462eebd1fcc1514
pedromagalhaes <pedro.magalhaes@outlook.com>
2024-08-28 11:06:36 +0100

    next-pwa

---
+++ .next/server/pages/_document.js
@@ -0,0 +1,11 @@
+"use strict";(()=>{var e={};e.id=660,e.ids=[660],e.modules={7471:(e,r,t)=>{t.r(r),t.d(r,{default:()=>n});var s=t(997),i=t(6859);function n(){return(0,s.jsxs)(i.Html,{lang:"en",dir:"ltr",children:[s.jsx(i.Head,{children:s.jsx("style",{children:`
+            html, body, #__next {
+              height: 100%;
+            }
+            #__next {
+              margin: 0 auto;
+            }
+            h1 {
+              text-align: center;
+            }
+            `})}),(0,s.jsxs)("body",{children:[s.jsx(i.Main,{}),s.jsx(i.NextScript,{})]})]})}},2785:e=>{e.exports=require("next/dist/compiled/next-server/pages.runtime.prod.js")},6689:e=>{e.exports=require("react")},997:e=>{e.exports=require("react/jsx-runtime")},1017:e=>{e.exports=require("path")}};var r=require("../webpack-runtime.js");r.C(e);var t=e=>r(r.s=e),s=r.X(0,[859],()=>t(7471));module.exports=s})();
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"Z085rc8BWRbCLSo1qzgxfHvQseSrcFwGyxKrFv7FORc=\"}"
