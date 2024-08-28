6bc6f710c28a28fb1e17006e82a8584e3adad404
pedromagalhaes <pedro.magalhaes@outlook.com>
2024-08-28 12:12:13 +0100

    init pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata, Viewport } from "next";
 import type { ReactNode } from "react";
 
-const APP_NAME = "next-pwa example";
-const APP_DESCRIPTION = "This is an example of using next-pwa";
+const APP_NAME = "pwa";
+const APP_DESCRIPTION = "example";
 
 export const metadata: Metadata = {
   applicationName: APP_NAME,
